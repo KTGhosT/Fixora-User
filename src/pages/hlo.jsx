a01a7a5f084561f219971e7dcf6d21382f691203
VaisnaviRajaratnam14 <rvaisnavi614@gmail.com>
2025-08-09 16:01:29 +0530

    New branch vaisu

---
+++ src/pages/hlo.jsx
@@ -0,0 +1,11 @@
+// MyComponent.jsx
+import React from "react";
+
+export default function MyComponent() {
+  return (
+    <div style={{ padding: "1rem", textAlign: "center" }}>
+      <h1>Hello, React!</h1>
+      <p>This is a simple JSX component.</p>
+    </div>
+  );
+}
